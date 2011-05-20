5e4c88aae69a78b4ccbe8449b74bf28bf451e496
martin <martin@hop.kcl.ac.uk>
2011-05-20 09:28:48 +0100

    pixel position

--- gauss.lisp
+++ gauss.lisp
@@ -1,6 +1,9 @@
 ;; 10.1.1.89.1895.pdf 2000Chen Robust Spot Fitting for Genetic Spot Array Images
 ;; fulltext.pdf Robust DNA microarray image analysis Norbert Braendle1, Horst Bischof2, Hilmar Lapp3
 (declaim (optimize (speed 1) (safety 1) (debug 1)))
+(defpackage :gauss
+  (:use :cl))
+(in-package :gauss)
 
 (deftype num ()
   `single-float)
@@ -78,7 +81,7 @@
 		   sum)))
     (/ top bottom)))
 
-(defun calc-position (w &key (h w))
+(defun calc-unit-position (w &key (h w))
   (declare (type fixnum w h))
   (let ((px (make-array (list h w) :element-type 'num))
 	(py (make-array (list h w) :element-type 'num)))
@@ -89,7 +92,15 @@
 	   (setf (aref px j i) a
 		 (aref py j i) b)))))
     (values px py)))
-
+(defun calc-pixel-position (w &key (h w))
+  (declare (type fixnum w h))
+  (let ((px (make-array (list h w) :element-type 'num))
+	(py (make-array (list h w) :element-type 'num)))
+    (dotimes (j h)
+      (dotimes (i w)
+	(setf (aref px j i) (* 1s0 i)
+	      (aref py j i) (* 1s0 j))))
+    (values px py)))
 (defun estimate-gauss (z px py)
   (declare (type (simple-array num 2) z px py)
 	   (values num num num num num &optional))
@@ -148,12 +159,14 @@
       (declare (type (simple-array num 2) zmodel))
      (/ top bottom))))
 
-(defun create (w h x y sxx sxy syy)
+(defun create (w h x y sxx sxy syy &key (position :pixel))
   (declare (type fixnum w h) 
 	   (type num x y sxx sxy syy)
 	   (values (simple-array num 2) &optional))
   (let ((ar (make-array (list h w) :element-type 'num)))
-    (multiple-value-bind (px py) (calc-position w :h h)
+    (multiple-value-bind (px py)(ecase position
+				   (:pixel (calc-pixel-position w :h h))
+				   (:unit (calc-unit-position w :h h)))
       (dotimes (j h)       
 	(dotimes (i w)
 	  (setf (aref ar j i)
@@ -162,25 +175,27 @@
 		   x y sxx sxy syy)))))
     ar))
 
-(defun create-default (w &key (h w) (x 0e0) (y 0e0) (sxx .1s0) (sxy 0s0) (syy sxx))
+(defun create-default (w &key (h w) (x 0e0) (y 0e0) (sxx .1s0) (sxy 0s0) (syy sxx) (position :pixel))
   (declare (type fixnum w h) 
 	   (type num x y sxx sxy syy))
-  (create w h x y sxx sxy syy))
+  (create w h x y sxx sxy syy :position position))
 
-(defun scale (q)
+(defun scale (q &key (s 9))
   (declare (type (simple-array num 2) q))
   (let* ((r (make-array (array-dimensions q)
 			:element-type '(unsigned-byte 8)))
 	 (r1 (sb-ext:array-storage-vector r))
 	 (q1 (sb-ext:array-storage-vector q)))
     (dotimes (i (length q1))
-      (setf (aref r1 i) (floor (* 9.5 (aref q1 i)))))
+      (setf (aref r1 i) (min 255 (max 0 (floor (* (+ s .5) (aref q1 i)))))))
     r))
 
-(defun do-fit (img)
+(defun do-fit (img &key (position :pixel))
   (declare (type (simple-array num 2) img))
   (destructuring-bind (h w) (array-dimensions img)
-    (multiple-value-bind (px py) (calc-position w :h h)
+    (multiple-value-bind (px py) (ecase position
+				   (:pixel (calc-pixel-position w :h h))
+				   (:unit (calc-unit-position w :h h)))
       (estimate-gauss img px py))))
 
 (defun run ()
@@ -195,7 +210,8 @@
 
 #+nil
 (scale
- (create-default 13 :sxx .04 :sxy .03))
+ (create-default 13 :x 2s0 :y 2s0 :sxx 4s0 :sxy .03
+		 ))
 
 
 (defun box-muller ()
@@ -234,10 +250,10 @@
 	 (setf p (* p (random 1s0))))
     (1- k)))
 #+nil
-(defparameter *bag* (let* ((n 1000000)
-			   (b (make-array n :element-type 'fixnum)))
+(defparameter *bag* (let* ((n 100000)
+			   (b (make-array n :element-type 'num)))
 		      (dotimes (i n) 
-			(setf (aref b i) (poisson-random-number 9s0)))
+			(setf (aref b i) (normal-random-number 19s0 200s0)))
 		      b))
 
 (defun histogram (a &key (n nil))
@@ -246,7 +262,7 @@
 	 (a-fix (make-array (length a) :element-type 'fixnum))
 	 (nn (if n
 		 n
-		 (- ma mi)))
+		 (floor (- ma mi))))
 	 (hist (make-array nn :element-type '(integer 0))))
     (dotimes (i (length a))
       (setf (aref a-fix i)
@@ -260,17 +276,43 @@
  (histogram *bag*))
 
 
-
 (defun print-histogram (hist mi ma)
   (let ((n (length hist)))
    (dotimes (i n)
-     (format t "~a ~a~%" (+ mi (* 1s0 (- ma mi) (/ i n))) (aref hist i)))))
+     (format t "~a~a~a~%" (+ mi (* 1s0 (- ma mi) (/ i n))) #\Tab (aref hist i)))))
 #+nil
 (with-open-file (*standard-output* "/dev/shm/o.dat" :if-does-not-exist :create
 				   :if-exists :supersede :direction :output)
  (multiple-value-call #'print-histogram
-   (histogram *bag*)))
+   (histogram *bag* :n 1000)))
 
+(defun write-pgm (filename img)
+  (declare (simple-string filename)
+           ((array (unsigned-byte 8) 2) img)
+           (values null &optional))
+  (destructuring-bind (h w)
+      (array-dimensions img)
+    (declare ((integer 0 65535) w h))
+    (with-open-file (s filename
+                       :direction :output
+                       :if-exists :supersede
+                       :if-does-not-exist :create)
+      (declare (stream s))
+      (format s "P5~%~D ~D~%255~%" w h))
+    (with-open-file (s filename 
+                       :element-type '(unsigned-byte 8)
+                       :direction :output
+                       :if-exists :append)
+      (let ((data-1d (make-array 
+                      (* h w)
+                      :element-type '(unsigned-byte 8)
+                      :displaced-to img)))
+        (write-sequence data-1d s)))
+    nil))
+(write-pgm "/dev/shm/o.pgm"
+	   (scale
+	    (create-default 130 :sxx .04 :sxy .03)
+	    :s 255))
 
 (print-histogram #(1 2 3) 1 3)
 (defun psi (a x)
